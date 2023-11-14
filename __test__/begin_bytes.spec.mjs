75aff34b39abc003a596676186ed0fbdbdb6b416
lopo <lopo@zju.edu.cn>
2023-11-15 00:16:44 +0800

    test: desc

--- __test__/begin_bytes.spec.mjs
+++ __test__/begin_bytes.spec.mjs
@@ -3,7 +3,7 @@ import { readFileSync } from "node:fs"
 import { resolvePath } from "./helper.mjs";
 import { findMime } from "../dist/index.mjs"
 
-test('begin bytes', t => {
+test('get mime from file extension begin bytes', t => {
     const bytes = readFileSync(resolvePath('./png.html')).slice(0, 20)
 
     t.is(findMime('png.html', bytes), 'image/png')
