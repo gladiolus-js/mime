033370beddacb7b4b4bd66ccc1bed80592329c22
lopo <lopo@zju.edu.cn>
2023-11-14 23:54:21 +0800

    fix: Array.isArray is too limited

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { findMimeByBeginBytes } from "./utils/begin_bytes";
+import { findMimeByBeginBytes, ToBeginBytes } from "./utils/begin_bytes";
 import { findMimeByExtension } from "./utils/extension";
 
 /**
@@ -8,17 +8,17 @@ function findMime(filename: string): string | null
 /**
  * Find mime type of file by begin bytes
  */
-function findMime(beginBytes: number[]): string | null
+function findMime(beginBytes: ToBeginBytes): string | null
 /**
  * Find mime type of file by filename or begin bytes.
  */
-function findMime(filename: string, beginBytes: number[]): string | null
+function findMime(filename: string, beginBytes: ToBeginBytes): string | null
 /**
  * Try to find the mime-type of file, if failed, return null.
  */
-function findMime(filenameOrBeginBytes: string | number[], beginBytes?: number[]): string | null {
+function findMime(filenameOrBeginBytes: string | ToBeginBytes, beginBytes?: ToBeginBytes): string | null {
     if(typeof filenameOrBeginBytes === 'string') {
-        if(Array.isArray(beginBytes)) {
+        if(!!beginBytes) {
             // use begin bytes first (if any), then filename if failed
             const beginBytesMime = findMimeByBeginBytes(beginBytes)
             if(beginBytesMime != null) return beginBytesMime
@@ -27,11 +27,9 @@ function findMime(filenameOrBeginBytes: string | number[], beginBytes?: number[]
             // use filename only
             return findMimeByExtension(filenameOrBeginBytes)
         }
-    } else if(Array.isArray(filenameOrBeginBytes)) {
+    } else {
         // use begin bytes only
         return findMimeByBeginBytes(filenameOrBeginBytes)
-    } else {
-        throw new TypeError('filenameOrBeginBytes must be string or number[]')
     }
 }
 
